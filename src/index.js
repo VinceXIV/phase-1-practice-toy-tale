1a2b69223a1580d6ec787f7afa4c12ff221ffc46
Vincent <vincent.makokha@student.moringaschool.com>
2022-08-22 17:36:59 +0300

    Add toy info to the card

--- src/index.js
+++ src/index.js
@@ -12,4 +12,28 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+
+  //Add toy info to the card
+
+  fetch('http://localhost:3000/toys')
+  .then(result => result.json())
+  .then(data => {
+    for(item of data){
+      document.getElementById('toy-collection')
+      .append(createCard(item))
+    }
+  })
 });
+
+
+function createCard(item){
+  const itemCard = document.createElement('div')
+  itemCard.classList.add('card')
+  itemCard.innerHTML = `<h2>${item.name}</h2>
+  <img src="${item.image}" class="toy-avatar" />
+  <p>${item.likes} Likes</p>
+  <button class="like-btn" id="${item.id}">Like ❤️</button>`
+
+  return itemCard
+}
